32b4ceeb58ddbe182294902dd9a28319b338b4c9
Chris Neal <voodoodaddy2000@gmail.com>
2019-10-29 17:46:53 -0700

    Cities are returning and showing Wx

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,5 +1,7 @@
+var apiKey = "dbf364bc39a4580a03a0dd92d999aa37";
+
 function getCityData(uaSlug) {
-    // var cityName = "Peoria%20Illinois";
+    console.log("I made it!")
     $.ajax({
         url: "https://api.teleport.org/api/urban_areas/slug:" + uaSlug + "/scores/",
         method: "GET"
@@ -9,30 +11,59 @@ function getCityData(uaSlug) {
 }
 
 //TELEPORTS AUTO COMPLETE
-var $results = document.querySelector('.results');
-var appendToResult = $results.insertAdjacentHTML.bind($results, 'afterend');
-TeleportAutocomplete.init('.my-input').on('change', function (value) {
-    appendToResult('<pre>' + JSON.stringify(value, null, 2) + '</pre>');
-    getCityData(value.uaSlug);
+var $cityOneResults = document.querySelector('#city-one-results');
+var $cityTwoResults = document.querySelector('#city-two-results');
+var cityOneResult = $cityOneResults.insertAdjacentHTML.bind($cityOneResults, 'afterend');
+var cityTwoResult = $cityTwoResults.insertAdjacentHTML.bind($cityTwoResults, 'afterend');
+
+TeleportAutocomplete.init('#city-choice-1').on('change', function (value) {
+    console.log(value);
+    cityOneResult('<pre>' + JSON.stringify(value, null, 2) + '</pre>');
+    getCityOneWx(value.latitude, value.longitude)
+    if (value.uaSlug) {
+        getCityData(value.uaSlug);
+    }
 });
 
-//TRYNG TO AUTO COMPLETE ON THE LIMITED LIST
-// var cityArray = JSON.parse("https://developers.teleport.org/assets/urban_areas.json");
-$( "#limitedCityInput" ).autocomplete({
-    source: citiesArray
-  });
+TeleportAutocomplete.init('#city-choice-2').on('change', function (value) {
+    console.log(value);
+    cityTwoResult('<pre>' + JSON.stringify(value, null, 2) + '</pre>');
+    getCityTwoWx(value.latitude, value.longitude)
+    if (value.uaSlug) {
+        getCityData(value.uaSlug);
+    }
+});
 
-// $('#limitedCityInput').autocomplete({
-//     source: function (request, response) {
-//         $.getJSON("https://developers.teleport.org/assets/urban_areas.json?term=" + request.term, function (data) {
-//             response($.map(data.dealers, function (value, key) {
-//                 return {
-//                     label: value,
-//                     value: key
-//                 };
-//             }));
-//         });
-//     },
-//     minLength: 2,
-//     delay: 100
-// });
+
+$cityOneName = $("#city-1-name");
+$cityOneIcon = $("#city-1-wx-icon");
+$cityOneTemp = $("#city-1-temp");
+$cityOneHumidity = $("#city-1-humidity");
+$cityTwoName = $("#city-2-name");
+$cityTwoIcon = $("#city-2-wx-icon");
+$cityTwoTemp = $("#city-2-temp");
+$cityTwoHumidity = $("#city-2-humidity");
+
+function getCityOneWx(lat, lon) {
+    $.ajax({
+        url:"https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey,
+        method: "GET"
+    }).then(function (response) {
+        //UPDATING THE CURRENT WEATHER FOR THE CURRENT CITY
+        $cityOneTemp.text(response.main.temp.toFixed(1));
+        $cityOneHumidity.text(response.main.humidity);
+        $cityOneIcon.attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+    })
+}
+
+function getCityTwoWx(lat, lon) {
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey,
+        method: "GET"
+    }).then(function (response) {
+        //UPDATING THE CURRENT WEATHER FOR THE CURRENT CITY
+        $cityTwoTemp.text(response.main.temp.toFixed(1));
+        $cityTwoHumidity.text(response.main.humidity);
+        $cityTwoIcon.attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+    })
+}
